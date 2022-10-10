3610fc66b68d3d44266bcfb904716796a7a08962
Nguyễn Hoàng Phúc <nguyenhoangphuc@MacBook-Pro-cua-Nguyen-2.local>
2022-10-10 07:18:47 +0700

    1

--- Main/index.js
+++ Main/index.js
@@ -1,4 +1,3 @@
-
 const entropy = (arr) => {
     let key
     let value
@@ -57,47 +56,97 @@ const inforGain = (arr, attribute) => {
         list: listFeatures
     }
 }
-const id3 = (arr, nameClass) => {
-    let index = 0
-    let max = -1;
-    let attribute
-    let e = entropy(arr)
-    
-    for (let item in arr[0]) {
-        if (index != 0 && index != Object.keys(arr[0]).length - 1) {
-            let {inforGain: value,list} = inforGain(arr, item)
-            if (max <= (e - value)) {
-                max = e - value
-                attribute = {
-                    nameAttribute: item,
-                    listFeatures: list
+const id3 = (arr, nameClass, parent) => {
+        let tree = new Object();
+    /////////////////////////////////////////
+        let table = '<div class="item" style="margin-top: 20px">'
+        let col = 'Xét tập dữ liệu D sau <table style="width: 30%" class="table table-striped table-bordered"><tr>'
+        
+        for(let item in arr[0]) {
+            if(item != parent)
+                col+= `<td>${item}</td>`
+        }
+        col+= "</tr>"
+        for(let item of arr) {
+            col+= `<tr>`
+            for(let i in item) {
+                if(i != parent)
+                    col+= `<td>${item[i]}</td>`
+            }
+            col+= `</tr>`
+
+        }
+        col+= "</table>"
+        table += col;
+    /////////////////////////////////////////
+        let index = 0
+        let max = -1;
+        let attribute
+        let e = entropy(arr)
+        table += `<p>Infor(D) = ${e}</p>`
+        let attriName = "<p>"
+        for (let item in arr[0]) {
+            if (index != 0 && index != Object.keys(arr[0]).length - 1 && item != parent) {
+                let {inforGain: value,list} = inforGain(arr, item)
+            /////////////////////////////////////////
+                 attriName += `- Gain(${item}) = `
+                attriName += Math.round((e-value)*1000)/1000 + "<br>"
+            /////////////////////////////////////////
+                if (max <= (e - value)) {
+                    max = e - value
+                    attribute = {
+                        nameAttribute: item,
+                        listFeatures: list
+                    }
                 }
             }
+            index++
+        } 
+        tree['attribute'] = new Object()
+        tree['attribute']['nameAttribute'] = attribute.nameAttribute
+        tree['attribute']['listValues'] = new Array()
+
+        
+        
+         /////////////////////////////////////////
+        attriName += `=>Chọn ${attribute.nameAttribute} làm thuộc tính phân hoạch</p>`
+        table += attriName
+        /////////////////////////////////////////
+    
+        document.querySelector(`.${nameClass}`).innerHTML += `<ul><li><div class="attribute">${attribute.nameAttribute}</div><ul class=${attribute.nameAttribute}></ul></li></ul>`
+    /////////////////////////////////////////
+        let listValue = 'Các giá trị của thuộc tính phân hoạch <table style="width: 20%" class="table table-striped table-bordered"><tbody><tr><th>Giá trị</th><th>Entropy</th><th>Class</th></tr>'
+        for (let i = 0; i < attribute.listFeatures.length; i++) {
+            tree['attribute']['listValues'].push(attribute.listFeatures[i].name)
+            listValue +=  `<tr><td>${attribute.listFeatures[i].name}</td>  <td>${attribute.listFeatures[i].infor}</td> <td>${attribute.listFeatures[i].res}</td></tr> </tr>`
+           
         }
-        index++
-    }
 
-    document.querySelector(`.${nameClass}`).innerHTML += `<ul><li><div class="attribute">${attribute.nameAttribute}</div><ul class=${attribute.nameAttribute}></ul></li></ul>`
-    for (let i of attribute.listFeatures) {
-       document.querySelector(`.${attribute.nameAttribute}`).innerHTML += `<li class="child ${i.name}"><div class="node">${i.name}</div></li>`
-        arrNext = (arr.filter(item => item[attribute.nameAttribute] == i.name))
-        arrNext.forEach(e => {
-            delete e[attribute.nameAttribute]
-        })
-        if (i.infor == 0) {
-            document.querySelector(`.${i.name}`).innerHTML += `<br><div style="margin-top: 10px"><span>${i.res}</span></div>`
-        } else {
-            id3(arrNext, i.name)
-
+        table += listValue
+        document.querySelector(".content").innerHTML += table
+    /////////////////////////////////////////
+    
+        for (let i of attribute.listFeatures) {
+           document.querySelector(`.${attribute.nameAttribute}`).innerHTML += `<li class="child ${i.name}"><div class="value">(${i.name})</div></li>`
+            arrNext = (arr.filter(item => item[attribute.nameAttribute] == i.name))
+          /*   arrNext.forEach(e => {
+                delete e[attribute.nameAttribute]
+            }) */
+            if (i.infor == 0) {
+               // document.querySelector(`.${i.name}`).innerHTML += `<br><div style="margin-top: 10px"><span>${i.res}</span></div>`
+                document.querySelector(`.${i.name}`).innerHTML += `<ul><li><div class="leaf">${i.res}</div></li></ul>`
+            } else {
+              id3(arrNext, i.name, attribute.nameAttribute)
+    
+            }
         }
+    
     }
-
-}
-
 const excel_file = document.getElementById('excel_file');
 excel_file.addEventListener('change', (event) => {
 let dataset = new Array()
     document.querySelector(".tree").innerHTML = ""
+    document.querySelector(".content").innerHTML = ""
     var reader = new FileReader();
     reader.readAsArrayBuffer(event.target.files[0]);
     reader.onload = function (event) {
@@ -120,11 +169,10 @@ let dataset = new Array()
                 }
                 dataset.push(object)
             }
-            id3(dataset, 'tree')
+            id3(dataset, 'tree', "")
 
         }
     }
 });
-setTimeout(function() {
-    console.log("Good Night!");
-  }, 1000);
+
+
